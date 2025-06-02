80ba3e0ad7d55b2bd47a20cce7075d9d0f8d20c9
shubham-unifyapps <shubham.singh@unifyapps.co.in>
2025-06-02 22:13:04 +0530

    Update identifier.js

--- identifier.js
+++ identifier.js
@@ -1,43 +1,52 @@
 
-import figmaData from "./figmaButton.js";
-
-
-
-
-
+import figmaData from "./figma.js"
 
 function isButtonLinkComponent(figmaData) {
   const nodes = figmaData?.result?.nodes || figmaData?.Result?.nodes;
   if (!nodes) return false;
 
   const nodeEntries = Object.entries(nodes);
-  if (nodeEntries.length === 0) return false;
+  
 
   const [_, mainNodeData] = nodeEntries[0];
   const mainNode = mainNodeData.document;
+ 
 
-  if (!mainNode || !Array.isArray(mainNode.children)) return false;
 
   const children = mainNode.children;
 
   const instanceNodes = children.filter(child => child.type === "INSTANCE");
+  const compNodes = children.filter(child => child.type === "COMPONENT");
+
   const textNodes = children.filter(child => child.type === "TEXT");
 
-  const hasOneText = textNodes.length === 1;
-  const hasOneOrTwoInstances = instanceNodes.length === 1 || instanceNodes.length === 2;
 
-  const typeCheck =  Object.values(nodes).some(node => {
-    const doc = node?.document;
-    return doc?.type === "INSTANCE" && doc?.name === "Buttons/Link";
-  });
+
+
+const firstNodeWithComponentSets = Object.values(nodes).find(
+  node => node.componentSets && typeof node.componentSets === "object"
+);
+
+
+const firstComponentSetName = firstNodeWithComponentSets
+  ? Object.values(firstNodeWithComponentSets.componentSets)?.[0]?.name
+  : null;
+
+const isButtonsLink = firstComponentSetName === "Buttons/Link";
+console.log(isButtonsLink);
+
+  
 
   const textContentCheck = textNodes[0]?.characters?.trim().length > 0;
 
   const instanceNamesCheck = instanceNodes.every(inst =>
-    typeof inst.name === "string" && /icon|clock/i.test(inst.name)
-  );   
-  
-  
+  typeof inst.name === "string" && /(icon|clock|Dot|Loading)/i.test(inst.name)
+);
+
+    const compCheck = compNodes.every(inst =>
+  typeof inst.name === "string" && /(icon|clock|Dot|Loading)/i.test(inst.name)
+);
+
 //   const doc = mainNodeData.document;                             
 //   const boundVars = doc?.boundVariables || {};
 //     const hasTrailingIcon = boundVars.iconTrailing?.value === true;
@@ -50,31 +59,12 @@ function isButtonLinkComponent(figmaData) {
 //       c.type === "VECTOR" && c.name === "clock"
 //     );
 
-
-
-  const componentProperties = mainNodeData.document.componentProperties ;
-    if (!componentProperties) return false;
-    const leadingIconKey = Object.keys(componentProperties).find((k) =>
-    k.includes("⬅️ Icon leading")
-  );
-  const trailingIconKey = Object.keys(componentProperties).find((k) =>
-    k.includes("➡️ Icon trailing")
-  );
-
-  if (
-    !leadingIconKey ||
-    !trailingIconKey ||
-    componentProperties[leadingIconKey].value !== true ||
-    componentProperties[trailingIconKey].value !== true
-  ) {
-    return false;
-  }
-  return hasOneText && hasOneOrTwoInstances && textContentCheck && instanceNamesCheck && typeCheck;
+  return isButtonsLink &&(instanceNamesCheck || compCheck) ;
 }
 
 if (isButtonLinkComponent(figmaData)) {
  
-  console.log("Valid Component! Output:");
+  console.log("Valid Component! ");
 } else {
   console.log("This Figma component is not a button-link structure.");
 }

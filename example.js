48d2189547d4a62d5ca8228e978a3ccc2f6f1023
shubham-unifyapps <shubham.singh@unifyapps.co.in>
2025-06-03 14:57:41 +0530

    Delete example.js

--- example.js
+++
@@ -1,7 +0,0 @@
-import figma_fetcher from './figmaDynamic.js';
-
-figma_fetcher('sm')
-.then (d => {
-    console.log(JSON.stringify(d, null, 2));
-})
-.catch(e => console.error('Error fetching Figma details:', e));
